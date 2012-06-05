ce6f030827ef285b0ffd7b75a5af722dc99e8137
Juan Pablo Marzetti <yonpols@gmail.com>
2012-06-05 14:42:59 -0400

    Add activation to the plugin

--- init.php
+++ init.php
@@ -1,10 +1,12 @@
 <?php
     Resources\MainController::$mountPoint = YPFramework::getSetting('resources.mount_point', 'resources');
+    if (YPFramework::getSetting('resources.active', true)) {
 
-    //Set the route settings according to the mount point set by the application settings
-    $route = new YPFObject;
-    $route->match = '/'.YPFramework::getFileName(Resources\MainController::$mountPoint, '$path');
-    $route->controller = 'resources\main';
-    $route->action = 'get';
-    YPFramework::setSetting('routes.resources_get', $route);
+        //Set the route settings according to the mount point set by the application settings
+        $route = new YPFObject;
+        $route->match = '/'.YPFramework::getFileName(Resources\MainController::$mountPoint, '$path');
+        $route->controller = 'resources\main';
+        $route->action = 'get';
+        YPFramework::setSetting('routes.resources_get', $route);
+    }
 ?>
